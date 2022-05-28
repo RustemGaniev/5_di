1a84bdd7bbe9156a8df04b52506874a666392d70
RustemGaniev <ganievrr@gmail.com>
2022-05-28 22:52:15 +0300

    di

---
+++ annotation/beans/src/main/java/ru/netology/servlet/MainServlet.java
@@ -0,0 +1,23 @@
+package ru.netology.servlet;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.netology.config.JavaConfig;
+import ru.netology.controller.PostController;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MainServlet extends HttpServlet {
+  private PostController controller;
+
+  @Override
+  public void init() {
+    final var applicationContext = new AnnotationConfigApplicationContext("ru.netology");
+    controller = applicationContext.getBean(PostController.class);
+  }
+
+
+  }
+
+
